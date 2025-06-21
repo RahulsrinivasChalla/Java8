8bec7270e27120eb973fc1389fa4a23ae5298dda
RahulsrinivasChalla <rahulsrinivas.c@outlook.com>
2025-06-21 12:57:35 +0530

    Update FunctionalInterfaces.java

--- Java8Features/src/com/jfs/functionalinterfaces/FunctionalInterfaces.java
+++ Java8Features/src/com/jfs/functionalinterfaces/FunctionalInterfaces.java
@@ -3,6 +3,14 @@ package com.jfs.functionalinterfaces;
 @FunctionalInterface
 public interface FunctionalInterfaces {
 	
-	public void hi() ;
+	public void apply() ;
+	
+	default public void log(){
+		System.out.println("Log");
+	}
+	
+	public static void info() {
+		System.out.println("Info");
+	}
 
 }
